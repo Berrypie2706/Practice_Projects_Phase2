db855263625b93cfe76825cb741a2eac8b9577e4
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-09-06 22:59:24 +0530

    Phase End Project

---
+++ Phase2EndProject/Phase2EndProject/Models/laptop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phase2EndProject.Models
+{
+    public class laptop
+    {
+        public string LaptopId { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public string Specification { get; set; }
+    }
+}
